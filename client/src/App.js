1e2c3b889f53edde4e2ce944e4cc311282cbc1ca
aritro66 <ghosharitro66@gmail.com>
2022-10-12 07:10:21 +0530

    Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import Products from "./pages/Product";
 import Disease from "./pages/Disease";
 import DiseaseInfo from "./pages/Disease/diseaseInfo";
 import ProductInfo from "./pages/Product/productInfo";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product" element={<Products />} />
