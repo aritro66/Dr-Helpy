b187ed723df72d64f836fd5113b90f911bbf6832
Aritro Ghosh <ghosharitro66@gmail.com>
2023-04-24 00:27:00 +0530

    c

--- server/test/test.js
+++ server/test/test.js
@@ -1,8 +1,15 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+// import { config } from "dotenv";
+const should = chai.should();
 const request = require("supertest");
 const server = require("../server.js");
 
+// config({
+//   path: "./.env",
+// });
+
+// const server = process.env.BASE_URL;
 chai.use(chaiHttp);
 
 // User authentication
@@ -12,6 +19,7 @@ describe("/POST Successful user authentication check", function () {
     process.exit(0);
   }, 20000);
   it("logging in the user", function (done) {
+    this.timeout(10000);
     request(server)
       .post("/login")
       .send({
@@ -20,6 +28,11 @@ describe("/POST Successful user authentication check", function () {
       })
       .end((err, res) => {
         res.should.have.status(200);
+        // res.body.should.be.a("object");
+        // res.body.should.be.have.property("success");
+        // res.body.success.should.equal(true);
+        // res.body.should.be.have.property("jwt_token");
+        // res.body.jwt_token.should.be.a("string");
         done();
       });
   });
